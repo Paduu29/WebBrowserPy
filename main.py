f8e77b7f411062546a7a2d2253c63afd81f9cc38
PaduU29 <zinelalexandru29@gmail.com>
2019-10-15 14:40:42 +0200

    add class AddressBar, also create toolbar and destroy tab on click

--- main.py
+++ main.py
@@ -8,28 +8,50 @@ from PyQt5.QtWidgets import(QApplication, QWidget, QVBoxLayout, QHBoxLayout,
 from PyQt5.QtGui import QIcon, QWindow, QImage
 from PyQt5.QtCore import *
 
+class AddressBar(QLineEdit):
+	def __init__(self):
+		super().__init__()
+		
+	def mousePressEvent(self, e):
+		self.selectAll()
+
 
 class App(QFrame):
 	def __init__(self):
 		super().__init__()
 		self.setWindowTitle("Web Browser")
-		self.setBaseSize(800, 600)
 		self.CreateApp()
+		self.setBaseSize(800, 600)
 		
 	def CreateApp(self):
 		self.layout = QVBoxLayout()
 		
-		self.tabbar = QTabBar()
-		
+		# Create Tabs
+		self.tabbar = QTabBar(movable=True, tabsClosable=True)
+		self.tabbar.tabCloseRequested.connect(self.CloseTab)
 		self.tabbar.addTab("Tab 1")
 		self.tabbar.addTab("Tab 2")
 		
 		self.tabbar.setCurrentIndex(0)
 		
+		#Create AddressBar
+		self.Toolbar = QWidget()
+		self.ToolbarLayout = QHBoxLayout()
+		self.addressbar = AddressBar()
+		
+		self.Toolbar.setLayout(self.ToolbarLayout)
+		self.ToolbarLayout.addWidget(self.addressbar)
 		
 		self.layout.addWidget(self.tabbar)
+		self.layout.addWidget(self.Toolbar)
+		
 		self.setLayout(self.layout)
+
 		self.show()
+		
+	def CloseTab(self, i):
+		self.tabbar.removeTab(i)
+		
 		
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
